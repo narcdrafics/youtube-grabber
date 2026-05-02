9ea9ca2b2d65691700cf2c05faa97936c56898c0
narcdrafics <expresiveuk@gmail.com>
2026-05-02 11:29:35 -0300

    chore: update firebase ignore patterns and enable standalone output mode for next.js deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   allowedDevOrigins: ["192.168.1.4", "localhost:3000", "localhost:3001"],
   images: {
     remotePatterns: [
