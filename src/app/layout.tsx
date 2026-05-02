2a74b80670daeadda6cdd1d35ec64c373a24e77e
narcdrafics <expresiveuk@gmail.com>
2026-05-02 10:37:45 -0300

    fix: correct casing for crossOrigin attribute in Adsense script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4061239001754518"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
           strategy="afterInteractive"
         />
       </head>
